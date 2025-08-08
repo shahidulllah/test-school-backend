775075ba60be7e0adef0d8ca124e5e65cde32056
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-08 21:43:09 +0600

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 dotenv.config();
 
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB Connection Failed", err);
+  });
