cbda45d7a4f4f269a903fc66556ae286bb3b5357
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-09 15:56:26 +0600

    added route to the app

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,25 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
-connectDB()
+connectDB();
 
 const app = express();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 //Routes
-
+app.use("/api/auth", authRoutes);
 
 //Checking response
-app.get("/", (_req, res) => res.send("Welcome to Test_School assesment platform..!!"));
+app.get("/", (_req, res) =>
+  res.send("Welcome to Test_School assesment platform..!!")
+);
 
 export default app;
