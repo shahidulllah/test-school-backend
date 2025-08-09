e99a9ab495d53d92ee4c15797f1866e87b306589
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-09 15:05:38 +0600

    user interface

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,18 @@
+export type UserRole = "admin" | "student" | "supervisor";
+
+export interface IUser {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  isVerified: boolean;
+  refreshTokens?: string[];
+  otp?: {
+    code: string;
+    expiresAt: Date;
+    purpose: "email_verification" | "password_reset" | "login";
+  } | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
